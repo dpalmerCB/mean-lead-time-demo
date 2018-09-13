e47fd1e20dd3994e11bda8cc8ede1f2d28335a98
Dan Palmer <dpalmer@cloudbees.com>
2018-09-13 10:26:39 -0400

    [TEST-1] Good change

--- src/main/java/loki/App.java
+++ src/main/java/loki/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println( "foo-1" );
+        System.out.println( "foot-1" );
         System.out.println( "foo-2" );
         System.out.println( "foo-3" );
         System.out.println( "foo-4" );
